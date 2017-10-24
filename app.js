7f1f7e815e6dc1d2e6a38419a0220295ec3a371e
Jordan Van Ness <jordanwvn@gmail.com>
2017-10-24 11:34:38 -0700

    made some changes

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 'use strict';
+
+var table = document.getElementById('table_content');
+var data = [];
+
+var cars = [{
+    make: 'honda',
+    model: 'accord',
+    year: 2011,
+    mileage: 87000
+  },
+  {
+    make: 'toyota',
+    model: 'corolla',
+    year: 1999,
+    mileage: 213000
+  },
+  {
+    make: 'ford',
+    model: 'escort',
+    year: 1993,
+    mileage: 9800
+  }];
+
+  for (var i = 0; i < cars.length; i++) {
+    data.push(
+      '<td>' + cars[i].make + '</td>' +
+      '<td>' + cars[i].model + '</td>' +
+      '<td>' + cars[i].year + '</td>' +
+      '<td>' + cars[i].mileage + '</td>'
+    )
+  }
+
+var newRow;
+
+for (var j = 0; j < data.length; j++) {
+  newRow = document.createElement('tr');
+  newRow.innerHTML = data[j];
+  table.appendChild(newRow);
+}
